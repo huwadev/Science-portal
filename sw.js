1de1594da9bb4179bf9838506e26656212317e92
huwadev <it@ethiosss.org>
2026-07-04 15:52:49 +0300

    Fix Cesium image loading error by base64 encoding SVGs and adding xmlns attribute

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'esss-science-portal-v7';
+const CACHE_NAME = 'esss-science-portal-v8';
 const RUNTIME_CACHE = 'esss-science-portal-runtime';
 
 // Assets to pre-cache immediately on service worker install
